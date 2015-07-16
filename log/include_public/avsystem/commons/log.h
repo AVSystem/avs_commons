1991a8070f6331d5ff156a8db6b2d7a2bc1528a5
Mateusz Kwiatkowski <m.kwiatkowski@avsystem.com>
2015-07-16 15:31:10 +0200

    Bootstrapped avs_log module

---
+++ log/include_public/avsystem/commons/log.h
@@ -0,0 +1,23 @@
+/*
+ * AVSystem Commons Library
+ *
+ * Copyright (C) 2015 AVSystem <http://www.avsystem.com/>
+ *
+ * This code is free and open source software licensed under the MIT License.
+ * See the LICENSE file for details.
+ */
+
+#ifndef AVS_COMMONS_LOG_H
+#define	AVS_COMMONS_LOG_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* AVS_COMMONS_LOG_H */
+
