26a144786fe03e94d29ed35fe82b46a1f48954b8
Mateusz Krawiec <m.krawiec@avsystem.com>
2016-11-03 14:02:21 +0100

    Added a few corner-case tests

--- algorithm/src/test/base64.c
+++ algorithm/src/test/base64.c
@@ -36,6 +36,12 @@ AVS_UNIT_TEST(base64, encode) {
     AVS_UNIT_ASSERT_EQUAL_STRING(result, "YQA=");
     AVS_UNIT_ASSERT_SUCCESS(avs_base64_encode("aa", 3, result, sizeof(result)));
     AVS_UNIT_ASSERT_EQUAL_STRING(result, "YWEA");
+    AVS_UNIT_ASSERT_SUCCESS(
+            avs_base64_encode("\x0C\x40\x03", 3, result, sizeof(result)));
+    AVS_UNIT_ASSERT_EQUAL_STRING(result, "DEAD");
+    /* output buffer too short */
+    AVS_UNIT_ASSERT_FAILED(
+            avs_base64_encode("\x0C\x40\x03\xAA", 4, result, sizeof(result)));
 }
 
 AVS_UNIT_TEST(base64, decode) {
@@ -51,6 +57,13 @@ AVS_UNIT_TEST(base64, decode) {
     AVS_UNIT_ASSERT_EQUAL(
             avs_base64_decode("YWEA", (uint8_t *) result, sizeof(result)), 3);
     AVS_UNIT_ASSERT_EQUAL_STRING(result, "aa");
+
+    AVS_UNIT_ASSERT_EQUAL(avs_base64_decode(
+            "Y== ==\n\n\t\vWEA", (uint8_t *) result, sizeof(result)), 3);
+    AVS_UNIT_ASSERT_EQUAL_STRING(result, "aa");
+
+    AVS_UNIT_ASSERT_EQUAL(
+            avs_base64_decode("", (uint8_t *) result, sizeof(result)), 0);
 }
 
 AVS_UNIT_TEST(base64, decode_fail) {
