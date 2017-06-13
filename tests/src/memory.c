8bf1e64c03261cc604c3abfea16875f48b6a8089
Mateusz Kwiatkowski <m.kwiatkowski@avsystem.com>
2017-06-13 12:36:19 +0200

    Fixed a bug in AVS_ALIGN_POINTER_INTERNAL__

--- tests/src/memory.c
+++ tests/src/memory.c
@@ -12,6 +12,42 @@
 #include <avsystem/commons/defs.h>
 #include <avsystem/commons/unit/test.h>
 
+AVS_UNIT_TEST(align_pointer, correct_alignment) {
+    AVS_UNIT_ASSERT_EQUAL(
+            (uintptr_t) AVS_ALIGN_POINTER_INTERNAL__(char *, (char *) 510, 4),
+            512);
+    AVS_UNIT_ASSERT_EQUAL(
+            (uintptr_t) AVS_ALIGN_POINTER_INTERNAL__(char *, (char *) 511, 4),
+            512);
+    AVS_UNIT_ASSERT_EQUAL(
+            (uintptr_t) AVS_ALIGN_POINTER_INTERNAL__(char *, (char *) 512, 4),
+            512);
+    AVS_UNIT_ASSERT_EQUAL(
+            (uintptr_t) AVS_ALIGN_POINTER_INTERNAL__(char *, (char *) 513, 4),
+            516);
+    AVS_UNIT_ASSERT_EQUAL(
+            (uintptr_t) AVS_ALIGN_POINTER_INTERNAL__(char *, (char *) 514, 4),
+            516);
+    AVS_UNIT_ASSERT_EQUAL(
+            (uintptr_t) AVS_ALIGN_POINTER_INTERNAL__(char *, (char *) 515, 4),
+            516);
+    AVS_UNIT_ASSERT_EQUAL(
+            (uintptr_t) AVS_ALIGN_POINTER_INTERNAL__(char *, (char *) 516, 4),
+            516);
+    AVS_UNIT_ASSERT_EQUAL(
+            (uintptr_t) AVS_ALIGN_POINTER_INTERNAL__(char *, (char *) 517, 4),
+            520);
+    AVS_UNIT_ASSERT_EQUAL(
+            (uintptr_t) AVS_ALIGN_POINTER_INTERNAL__(char *, (char *) 518, 4),
+            520);
+    AVS_UNIT_ASSERT_EQUAL(
+            (uintptr_t) AVS_ALIGN_POINTER_INTERNAL__(char *, (char *) 519, 4),
+            520);
+    AVS_UNIT_ASSERT_EQUAL(
+            (uintptr_t) AVS_ALIGN_POINTER_INTERNAL__(char *, (char *) 520, 4),
+            520);
+}
+
 AVS_UNIT_TEST(aligned_stack_allocation, correct_alignment) {
     AVS_ALIGNED_STACK_BUF(A, 42);
     AVS_UNIT_ASSERT_TRUE(((unsigned long) A) % sizeof(avs_max_align_t) == 0);
