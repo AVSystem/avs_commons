64721547241675adddc11749a6b81df05afd4a8a
Marcin Radomski <m.radomski@avsystem.com>
2017-07-11 15:27:41 +0200

    Refactor _avs_unit_assert to a macro
    Summary:
    By having the `if (condition)` part inlined and explicitly marking the
    `_avs_unit_assert_fail` noreturn, we prevent scan-build (and probably
    other analyzers too) from thinking that a failed assertion does not
    prevent a test from continuing.
    
    Test Plan: make analyze on Anjay
    
    Reviewers: kfyatek!, mkrawiec!
    
    Differential Revision: https://phabricator.avsystem.com/D4908

--- unit/src/test.h
+++ unit/src/test.h
@@ -6,10 +6,16 @@ void _avs_unit_test_fail_printf(const char *file,
                                 const char *format,
                                 ...);
 
-void _avs_unit_assert(int condition,
-                      const char *file,
-                      int line,
-                      const char *format,
-                      ...);
+void _avs_unit_assert_fail(const char *file,
+                           int line,
+                           const char *format,
+                           ...);
+
+#define _avs_unit_assert(Condition, File, Line, ...) \
+    do { \
+        if (!(Condition)) { \
+            _avs_unit_assert_fail((File), (Line), __VA_ARGS__); \
+        } \
+    while (0)
 
 #endif /* AVS_COMMONS_UNIT_SRC_TEST_H */
