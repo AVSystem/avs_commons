888fab8e889421e840152bdcca953e1febaf146a
Mateusz Krawiec <m.krawiec@avsystem.com>
2017-08-02 11:30:50 +0200

    Fix error check in avs_simple_snprintf()

--- utils/src/format.c
+++ utils/src/format.c
@@ -20,5 +20,5 @@ int avs_simple_snprintf(char *out, size_t out_size, const char *format, ...) {
     va_start(args, format);
     int result = vsnprintf(out, out_size, format, args);
     va_end(args);
-    return (result >= 0 && (size_t) result >= out_size) ? -1 : 0;
+    return (result < 0 || (size_t) result >= out_size) ? -1 : 0;
 }
