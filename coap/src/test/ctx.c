7009693e7bfed5c7e360dc1e835f1c178c47fa21
Mateusz Krawiec <m.krawiec@avsystem.com>
2017-08-29 10:27:04 +0200

    Fix commented tests in avs_commons.

--- coap/src/test/ctx.c
+++ coap/src/test/ctx.c
@@ -39,12 +39,18 @@
 
 #define COAP_MSG_MAX_SIZE 1152
 
-typedef struct {
-    uint16_t port;
-    pid_t pid;
-} dtls_server_t;
+typedef enum {
+    TYPE_DTLS,
+    TYPE_UDP
+} socket_type_t;
 
-static AVS_LIST(dtls_server_t) dtls_servers;
+typedef struct {
+    pid_t pid;
+    uint16_t port;
+} server_t;
+
+static AVS_LIST(server_t) dtls_servers;
+static AVS_LIST(server_t) udp_servers;
 
 static void set_sigusr1_mask(int action) {
     sigset_t set;
@@ -64,10 +70,13 @@ static void kill_servers(void) {
     AVS_LIST_CLEAR(&dtls_servers) {
         kill(dtls_servers->pid, SIGTERM);
     }
+    AVS_LIST_CLEAR(&udp_servers) {
+        kill(udp_servers->pid, SIGTERM);
+    }
 }
 
 static void spawn_dtls_echo_server(uint16_t port) {
-    dtls_server_t *serv;
+    server_t *serv;
     AVS_LIST_FOREACH(serv, dtls_servers) {
         if (serv->port == port) {
             LOG(ERROR, "another server running on port %u", port);
@@ -77,7 +86,9 @@ static void spawn_dtls_echo_server(uint16_t port) {
     }
 
     char port_string[6];
-    AVS_UNIT_ASSERT_TRUE(avs_simple_snprintf(port_string, sizeof(port_string), "%u", port) >= 0);
+    AVS_UNIT_ASSERT_TRUE(
+            avs_simple_snprintf(port_string, sizeof(port_string), "%u", port)
+            >= 0);
 
     char *cmdline[] = {
         AVS_TEST_BIN_DIR "/../tools/dtls_echo_server",
@@ -100,7 +111,8 @@ static void spawn_dtls_echo_server(uint16_t port) {
         // fall-through
     case -1:
         LOG(ERROR, "could not start DTLS echo server: %s", strerror(errno));
-        LOG(ERROR, "command: %s %s %s %s", cmdline[0], cmdline[1], cmdline[2], cmdline[3]);
+        LOG(ERROR, "command: %s %s %s %s", cmdline[0], cmdline[1], cmdline[2],
+            cmdline[3]);
         abort();
     default:
         break;
@@ -108,7 +120,7 @@ static void spawn_dtls_echo_server(uint16_t port) {
 
     atexit(kill_servers);
 
-    serv = AVS_LIST_NEW_ELEMENT(dtls_server_t);
+    serv = AVS_LIST_NEW_ELEMENT(server_t);
     AVS_UNIT_ASSERT_NOT_NULL(serv);
     serv->pid = pid;
     serv->port = port;
@@ -118,8 +130,131 @@ static void spawn_dtls_echo_server(uint16_t port) {
     set_sigusr1_mask(SIG_UNBLOCK);
 }
 
-static avs_net_abstract_socket_t *setup_dtls_socket(uint16_t port) {
-    spawn_dtls_echo_server(port);
+static ssize_t
+udp_echo(const char *in, size_t in_size, char *out, size_t out_size) {
+    if (in_size > out_size) {
+        return -1;
+    }
+
+    memcpy(out, in, in_size);
+    return (ssize_t)in_size;
+}
+
+static void udp_echo_serve(uint16_t port) {
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    if (bind(sock, (struct sockaddr*)&addr, sizeof(addr))) {
+        LOG(ERROR, "UDP server (127.0.0.1:%u) bind failed: %s", port,
+            strerror(errno));
+        goto cleanup;
+    }
+
+    // notify parent
+    kill(getppid(), SIGUSR1);
+
+    char in_buffer[65535];
+    char out_buffer[65535];
+    struct pollfd sock_pollfd = {
+        .fd = sock,
+        .events = POLLIN,
+        .revents = 0
+    };
+
+    while (true) {
+        if (poll(&sock_pollfd, 1, -1) < 0) {
+            LOG(ERROR, "UDP server (127.0.0.1:%u) poll failed: %s", port,
+                strerror(errno));
+            goto cleanup;
+        }
+
+        struct sockaddr_in remote_addr;
+        memset(&remote_addr, 0, sizeof(remote_addr));
+        socklen_t remote_addr_len = sizeof(remote_addr);
+
+        ssize_t bytes_recv = recvfrom(sock, in_buffer, sizeof(in_buffer), 0,
+                                      (struct sockaddr*)&remote_addr,
+                                      &remote_addr_len);
+        if (bytes_recv < 0) {
+            LOG(ERROR, "UDP server (127.0.0.1:%u) recvfrom failed: %s",
+                     port, strerror(errno));
+            goto cleanup;
+        }
+
+        ssize_t bytes_to_send = udp_echo(in_buffer, (size_t) bytes_recv,
+                                         out_buffer, sizeof(out_buffer));
+        if (bytes_to_send < 0) {
+            LOG(ERROR, "UDP server (127.0.0.1:%u) udp_echo failed",
+                     port);
+            goto cleanup;
+        }
+
+        if (sendto(sock, out_buffer, (size_t)bytes_to_send, 0,
+                   (struct sockaddr*)&remote_addr, remote_addr_len) != bytes_to_send) {
+            LOG(ERROR, "UDP server (127.0.0.1:%u) sendto failed: %s",
+                     port, strerror(errno));
+            goto cleanup;
+        }
+    }
+
+cleanup:
+    close(sock);
+    LOG(ERROR, "UDP server (127.0.0.1:%u) shutting down", port);
+}
+
+static void spawn_udp_echo_server(uint16_t port) {
+    server_t *serv;
+    AVS_LIST_FOREACH(serv, udp_servers) {
+        if (serv->port == port) {
+            LOG(ERROR, "another server running on port %u", port);
+            abort();
+        }
+    }
+
+    set_sigusr1_mask(SIG_BLOCK);
+
+    int pid = -1;
+    switch (pid = fork()) {
+    case 0:
+#if __linux__
+        if (prctl(PR_SET_PDEATHSIG, SIGHUP)) {
+            LOG(WARNING, "prctl failed: %s", strerror(errno));
+        }
+#endif // __linux__
+        udp_echo_serve(port);
+        // fall-through
+    case -1:
+        LOG(ERROR, "could not start UDP server on port %u: %s", port,
+            strerror(errno));
+        abort();
+    default:
+        break;
+    }
+
+    atexit(kill_servers);
+
+    serv = AVS_LIST_NEW_ELEMENT(server_t);
+    AVS_UNIT_ASSERT_NOT_NULL(serv);
+    serv->pid = pid;
+    serv->port = port;
+    AVS_LIST_INSERT(&udp_servers, serv);
+
+    wait_for_child();
+    set_sigusr1_mask(SIG_UNBLOCK);
+}
+
+static avs_net_abstract_socket_t *setup_socket(socket_type_t type,
+                                               uint16_t port) {
+    switch (type) {
+    case TYPE_DTLS: spawn_dtls_echo_server(port); break;
+    case TYPE_UDP: spawn_udp_echo_server(port); break;
+    }
+
+    bool use_nosec = (type == TYPE_UDP);
     avs_net_abstract_socket_t *backend = NULL;
     static const char *ROOT_CRT_FILE = AVS_TEST_BIN_DIR "/certs/root.crt";
     static const char *CLIENT_CRT_FILE = AVS_TEST_BIN_DIR "/certs/client.crt";
@@ -154,8 +289,12 @@ static avs_net_abstract_socket_t *setup_dtls_socket(uint16_t port) {
     AVS_UNIT_ASSERT_TRUE(
             avs_simple_snprintf(port_str, sizeof(port_str), "%u", port) >= 0);
 
+    avs_net_socket_type_t sock_type = use_nosec ? AVS_NET_UDP_SOCKET
+                                                : AVS_NET_DTLS_SOCKET;
+    void *sock_config = use_nosec ? (void *) &config.backend_configuration
+                                  : (void *) &config;
     AVS_UNIT_ASSERT_SUCCESS(
-            avs_net_socket_create(&backend, AVS_NET_DTLS_SOCKET, &config));
+            avs_net_socket_create(&backend, sock_type, sock_config));
     // this doesn't actually do anything,
     // but ensures that bind() and connect() can be used together
     AVS_UNIT_ASSERT_SUCCESS(avs_net_socket_bind(backend, NULL, NULL));
@@ -164,14 +303,13 @@ static avs_net_abstract_socket_t *setup_dtls_socket(uint16_t port) {
     return backend;
 }
 
-#if 0
 AVS_UNIT_TEST(coap_ctx, coap_udp) {
     avs_net_socket_opt_value_t mtu;
     // udp_client_send_recv
     avs_coap_ctx_t *ctx = NULL;
     AVS_UNIT_ASSERT_SUCCESS(avs_coap_ctx_create(&ctx, 0));
 
-    avs_net_abstract_socket_t *backend = setup_dtls_socket(TEST_PORT_DTLS);
+    avs_net_abstract_socket_t *backend = setup_socket(TYPE_UDP, TEST_PORT_UDP);
 
     avs_coap_msg_info_t info = avs_coap_msg_info_init();
     info.type = AVS_COAP_MSG_CONFIRMABLE;
@@ -207,14 +345,14 @@ AVS_UNIT_TEST(coap_ctx, coap_udp) {
     free(storage);
     avs_coap_ctx_cleanup(&ctx);
 }
-#endif
 
 AVS_UNIT_TEST(coap_ctx, coap_dtls) {
     avs_net_socket_opt_value_t mtu;
     avs_coap_ctx_t *ctx = NULL;
     AVS_UNIT_ASSERT_SUCCESS(avs_coap_ctx_create(&ctx, 0));
 
-    avs_net_abstract_socket_t *backend = setup_dtls_socket(TEST_PORT_DTLS);
+    avs_net_abstract_socket_t *backend =
+            setup_socket(TYPE_DTLS, TEST_PORT_DTLS);
 
     avs_coap_msg_info_t info = avs_coap_msg_info_init();
     info.type = AVS_COAP_MSG_CONFIRMABLE;
