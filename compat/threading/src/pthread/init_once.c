d8461fae3a1d5cf6990e4daa4732f304e6deff60
Marcin Radomski <m.radomski@avsystem.com>
2018-06-21 10:20:23 +0200

    Use recursive mutex in pthread/init_once

--- compat/threading/src/pthread/init_once.c
+++ compat/threading/src/pthread/init_once.c
@@ -27,12 +27,48 @@
 
 VISIBILITY_SOURCE_BEGIN
 
-static pthread_mutex_t g_mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t g_mutex;
+static volatile bool g_mutex_initialized = false;
+
+/*
+ * PTHREAD_RECURSIVE_MUTEX_INITIALIZER is non-portable; the only method of
+ * creating a recursive mutex defined by POSIX is to use pthread_mutex_init()
+ * with appropriate attributes
+ */
+static int ensure_mutex_initialized(void) {
+    if (g_mutex_initialized) {
+        return 0;
+    }
+
+    static pthread_mutex_t mutex_init_mutex = PTHREAD_MUTEX_INITIALIZER;
+    if (pthread_mutex_lock(&mutex_init_mutex)) {
+        return -1;
+    }
+
+    int result = 0;
+
+    if (!g_mutex_initialized) {
+        pthread_mutexattr_t attr;
+        ((void) ((result = pthread_mutexattr_init(&attr))
+                 || (result = pthread_mutexattr_settype(
+                         &attr, PTHREAD_MUTEX_RECURSIVE))
+                 || (result = pthread_mutex_init(&g_mutex, &attr))));
+
+        if (result == 0) {
+            g_mutex_initialized = true;
+        }
+
+    }
+
+    pthread_mutex_unlock(&mutex_init_mutex);
+    return result;
+}
 
 int avs_init_once(volatile avs_init_once_handle_t *handle,
                   avs_init_once_func_t *func,
                   void *func_arg) {
-    if (pthread_mutex_lock(&g_mutex)) {
+    if (ensure_mutex_initialized()
+            || pthread_mutex_lock(&g_mutex)) {
         return -1;
     }
 
