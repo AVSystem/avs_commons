4e86a4423ddb0b0ef0e2211bc1d48f428e14ca09
Kamil Panek <k.panek@avsystem.com>
2019-05-30 10:02:54 +0200

    Remove warnings about session resumption
    Summary: To keep `-Werror` in CoAP
    
    Test Plan: `make check`
    
    Reviewers: marian!, mkrawiec!, kfyatek!, molczyk
    
    Differential Revision: https://phabricator.avsystem.com/D8362

--- net/src/openssl/openssl.c
+++ net/src/openssl/openssl.c
@@ -664,7 +664,6 @@ static bool is_ssl_started(ssl_socket_t *socket) {
     return socket->ssl != NULL;
 }
 
-#warning "TODO: Session resumption support"
 static bool is_session_resumed(ssl_socket_t *socket) {
     (void) socket;
     return false;
