e3a8f8b990b395459a218a32331a5a846184a9f6
Mateusz Kwiatkowski <m.kwiatkowski@avsystem.com>
2019-06-14 15:30:40 +0200

    Rewritten check-extern-c in Python

---
+++ tools/check-extern-c.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017-2019 AVSystem <avsystem@avsystem.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import os
+import re
+import sys
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+
+def has_proper_extern_c_clause(filename):
+    with open(filename) as f:
+        for line in f:
+            if 'extern "C"' in line:
+                return True
+    return False
+
+
+def _main():
+    parser = argparse.ArgumentParser(
+        description='Check if all public headers contain an extern "C" clause')
+    parser.add_argument('-p', '--path', help='Project root path to start checking in',
+                        default=PROJECT_ROOT)
+    parser.add_argument('-r', '--regex',
+                        help='Regular expression that matches all files that need to be checked',
+                        default=r'/include_public/.*\.h$')
+    args = parser.parse_args()
+
+    regex = re.compile(args.regex)
+
+    failure = False
+
+    for root, dirs, files in os.walk(args.path):
+        if '.git' in files:
+            # ".git" file that is not a subdirectory means that most likely
+            # we're in a submodule directory - do not iterate further
+            dirs.clear()
+            continue
+        try:
+            dirs.remove('.git')
+        except ValueError:
+            pass
+
+        for filename in files:
+            full_filename = os.path.join(root, filename)
+            if regex.search(full_filename) is not None:
+                if not has_proper_extern_c_clause(full_filename):
+                    failure = True
+                    sys.stderr.write('Missing extern "C" conditional in %s\n' % (full_filename,))
+
+    if failure:
+        return 1
+
+
+if __name__ == '__main__':
+    sys.exit(_main())
