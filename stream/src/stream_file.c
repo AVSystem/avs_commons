101eec172e05c3b137d4dbc54a3a87fd5cd73f2c
Kamil Panek <k.panek@avsystem.com>
2019-07-01 11:56:37 +0200

    Fix bug in stream_file_read
    Summary: ...
    
    Test Plan: none
    
    Reviewers: marian, mkrawiec, kfyatek, molczyk
    
    Differential Revision: https://phabricator.avsystem.com/D8509

--- stream/src/stream_file.c
+++ stream/src/stream_file.c
@@ -103,8 +103,9 @@ static int stream_file_read(avs_stream_abstract_t *stream_,
         file->error_code = EBADF;
         return -1;
     }
+    size_t bytes_read = fread(buffer, 1, buffer_length, file->fp);
     if (out_bytes_read) {
-        *out_bytes_read = fread(buffer, 1, buffer_length, file->fp);
+        *out_bytes_read = bytes_read;
     }
     if (ferror(file->fp)) {
         file->error_code = EIO;
